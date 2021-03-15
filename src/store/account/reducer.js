1924ddc5b87ea44749373dcd7e9965759fe813b4
Domenico Gemoli <domenicogemoli@gmail.com>
2021-03-15 13:44:51 +0100

    Add redux store

---
+++ src/store/account/reducer.js
@@ -0,0 +1,12 @@
+// src/store/account/reducer.js
+const initialState = {
+  someProperty: "Some initial value",
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
